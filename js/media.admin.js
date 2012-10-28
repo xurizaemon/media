0545d7d19ae40170463c6c0328ad67c014e82ba8
Paris Liakos <rootatwc@gmail.com>
2012-10-28 03:41:09 +0200

    Remove obsolete media types javascript

--- js/media.admin.js
+++ js/media.admin.js
@@ -74,27 +74,4 @@ Drupal.behaviors.mediaAdmin = {
   }
 };
 
-/**
- * JavaScript for the Media types administrative form.
- */
-Drupal.behaviors.mediaTypesAdmin = {
-  attach: function (context) {
-    if ($('.form-item-match-type', context).length == 0) {
-      return;
-    }
-    // Toggle the 'other' text field on Match type.
-    if ($('.form-item-match-type input:checked').val() != 'other') {
-      $('.form-item-match-type-other').hide();
-    }
-    $('.form-item-match-type input').change(function () {
-      if ($(this).val() == 'other') {
-        $('.form-item-match-type-other').slideDown('fast');
-      }
-      else {
-        $('.form-item-match-type-other').slideUp('fast');
-      }
-    });
-  }
-};
-
 })(jQuery);
