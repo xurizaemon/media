e771336e3eab2389316d20d2c73a3f69ceef155b
Paris Liakos <rootatwc@gmail.com>
2013-01-31 10:03:09 +0200

    Issue #1743040 by arthurf, rootatwc, aaronbauman: "You have not selected anything!" error message, when a selection has clearly been made

--- js/plugins/media.views.js
+++ js/plugins/media.views.js
@@ -75,7 +75,7 @@ Drupal.media.browser.views.setup = function(view) {
 
   // Catch the click on a media item
   $('.view-content .media-item', view).bind('click', function () {
-    var fid = $(this).closest('a[data-fid]').data('fid'),
+    var fid = $(this).closest('.media-item[data-fid]').data('fid'),
       selectedFiles = new Array();
 
     // Remove all currently selected files
@@ -106,7 +106,7 @@ Drupal.media.browser.views.setup = function(view) {
           selectedFiles.push(Drupal.media.browser.selectedMedia[index]);
 
           // Mark it as selected
-          $('.view-content *[data-fid=' + currentFid + '] .media-item', view).addClass('selected');
+          $('.view-content *[data-fid=' + currentFid + '].media-item', view).addClass('selected');
         }
       }
     }
